bb97fc297f7a1fb3846cf5a5e8e83ee9a79a16af
Daniel Ortega <dsantiagortega@hotmail.com>
2021-03-27 16:26:38 -0500

    Agrego enum hotel

---
+++ src/model/Hotel.java
@@ -0,0 +1,16 @@
+package model;
+
+public enum Hotel {
+    HOTEL_A("Hotel a"), HOTEL_B("Hotel b");
+
+    private String hotel;
+
+    Hotel(String name) {
+        hotel = name;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+}
