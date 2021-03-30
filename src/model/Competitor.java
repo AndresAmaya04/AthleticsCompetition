0058b74a4b6445ba099e1902ed2caf5f76790237
Daniel Ortega <dsantiagortega@hotmail.com>
2021-03-29 22:01:22 -0500

    Creo atributos en Competitor, constructor y getters

--- src/model/Competitor.java
+++ src/model/Competitor.java
@@ -1,6 +1,69 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Competitor {
+    private String name;
+    private String lastName;
+    private Gender gender;
+    private LocalDate dateOfBirth;
+    private Delegation delegation;
+    private String id;
+    private int dorsal;
+    private Competence competences;
+    private Category category;
+    public ScoreType scoreType;
 
+    public Competitor(String name, String lastName, Gender gender, LocalDate dateOfBirth, Delegation delegation, String id, int dorsal, Competence competences, Category category, ScoreType scoreType) {
+        this.name = name;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.delegation = delegation;
+        this.id = id;
+        this.dorsal = dorsal;
+        this.competences = competences;
+        this.category = category;
+        this.scoreType = scoreType;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Delegation getDelegation() {
+        return delegation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public Competence getCompetences() {
+        return competences;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public ScoreType getScoreType() {
+        return scoreType;
+    }
 }
